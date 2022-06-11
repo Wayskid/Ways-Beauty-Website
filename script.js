7e2526ac3c8e878f835e8daa8cc73f0b17e1b368
Wayskid <wayskid02@gmail.com>
2022-06-11 09:51:46 -0500

    Mobile Version Now Available

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-
+const desktopQuery = window.matchMedia("(min-width:900px)");
+const mobileQuery = window.matchMedia("(max-width:900px)");
 const blueBkgClr = document.querySelector(".blueBkgClr");
 const burger = document.querySelector(".burger");
 const bar1 = document.querySelector(".bar1");
@@ -15,6 +16,7 @@ const facebook = document.querySelector(".facebook");
 const twitter = document.querySelector(".twitter");
 const instagram = document.querySelector(".instagram");
 const bkgVideo = document.querySelector(".bkgVideo");
+const mobileMenu = document.querySelector(".mobileMenu");
 
 
 
@@ -26,6 +28,8 @@ arrow.addEventListener("click", () => {
 
 
 burger.addEventListener("click", () => {
+if (desktopQuery.matches){
+        
 
         bar1.classList.toggle("cross");
         bar2.classList.toggle("cross");
@@ -43,6 +47,25 @@ burger.addEventListener("click", () => {
         arrow.classList.toggle("hideArrow");
 
         
-});
+}else if(mobileQuery.matches){
+
+        bar1.classList.toggle("cross");
+        bar2.classList.toggle("cross");
+        bar3.classList.toggle("cross");
+        mobileMenu.classList.toggle("showMobileMenu");
+        blueBkgClr.classList.toggle("open");
+        navbar.classList.toggle("showNavbar");
+        facebook.classList.toggle("showFacebook");
+        twitter.classList.toggle("showTwitter");
+        instagram.classList.toggle("showIG");
+        search.classList.toggle("showSearch");
+        mainBody1.classList.toggle("hideMain");
+        mainBody2.classList.toggle("hideMain");
+        bkgVideo.classList.toggle("bkgVideoBlur");
+        arrow.classList.toggle("hideArrow");
+        
+}})
+
+
 
 
